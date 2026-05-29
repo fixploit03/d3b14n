03920ba8ddef4e4250e092df6a3daed333554ff2
fixploit03 <164528883+fixploit03@users.noreply.github.com>
2026-05-29 09:03:40 +0700

    Create konfigurasi_network.sh

---
+++ src/konfigurasi_network.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+config=/etc/network/interfaces
+
+# backup
+cp "${config}" "${config}.bak"
+
+cat <<EOF> "${config}"
+# This file describes the network interfaces available on your system
+# and how to activate them. For more information, see interfaces(5).
+
+source /etc/network/interfaces.d/*
+
+# The loopback network interface
+auto lo
+iface lo inet loopback
+
+# The primary network interface
+allow-hotplug eth0
+iface eth0 inet dhcp
+
+# This is an autoconfigured IPv6 interface
+#iface eth0 inet6 auto
+EOF
+
+systemctl restart networking
