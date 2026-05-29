ec1dfb2ee83eba6200f841818b3b7b2da62908d8
fixploit03 <164528883+fixploit03@users.noreply.github.com>
2026-05-30 06:11:22 +0700

    Update post_install.sh

--- src/post_install.sh
+++ src/post_install.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+path_saat_ini=$(pwd)
 path_apps=../apps
 path_bin=/usr/local/bin
 
+#--------------
+# archiver
+#--------------
+apt install -y zip unzip 7zip
+#
 # rar
 cd "${path_apps}"
 tar -zxf rarlinux-x64-722.tar.gz
@@ -12,3 +18,4 @@ mv unrar "${path_bin}"
 cd ..
 rm -rf rar
 rm rarlinux-x64-722.tar.gz
+cd "${path_saat_ini}"
