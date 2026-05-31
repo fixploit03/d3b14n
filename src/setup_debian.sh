2d288b891935f4255b87e02f7c1580fff6ca33ec
fixploit03 <164528883+fixploit03@users.noreply.github.com>
2026-05-31 19:32:05 +0700

    Update setup_debian.sh

--- src/setup_debian.sh
+++ src/setup_debian.sh
@@ -9,4 +9,10 @@ bash setup/konfigurasi_sudo.sh
 bash setup/konfigurasi_path_environment_variable.sh
 bash setup/konfigurasi_network.sh
 bash setup/konfigurasi_bashrc.sh
+bash setup/instal_utils.sh
+bash setup/instal_archiver.sh
+bash setup/instal_tampilan.sh
+bash setup/instal_terminal.sh
+bash setup/instal_virtualbox_guest_additions.sh
 bash setup/konfigurasi_grub.sh
+bash setup/selesai.sh
