c67ae547eb814caf49e1828b6a150af1dd87fbb7
fixploit03 <164528883+fixploit03@users.noreply.github.com>
2026-05-31 18:35:06 +0700

    Update post_install.sh

--- src/post_install.sh
+++ src/post_install.sh
@@ -40,21 +40,6 @@ sudo apt install -y terminator fonts-jetbrains-mono
 sudo apt install -y numix-gtk-theme papirus-icon-theme numix-icon-theme
 
 #--------------
-# 802.11
-#--------------
-sudo apt install -y wireless-tools iw aircrack-ng pixiewps reaver bully mdk3 mdk4 macchanger cowpatty hcxdumptool hcxtools hcxkeys bettercap wifite wireshark tshark horst wavemon
-#
-# hostapd-wpe
-sudo dpkg -i "${path_apps}/80211/libunsafessl1.0.2_1.0.2u-0kali2_amd64.deb" "${path_apps}/80211/hostapd-wpe_2.10+git20220310-0kali3_amd64.deb"
-sudo apt install -y -f
-#
-# kismet
-wget -O - https://www.kismetwireless.net/repos/kismet-release.gpg.key --quiet | gpg --dearmor | sudo tee /usr/share/keyrings/kismet-archive-keyring.gpg >/dev/null
-echo 'deb [signed-by=/usr/share/keyrings/kismet-archive-keyring.gpg] https://www.kismetwireless.net/repos/apt/release/trixie trixie main' | sudo tee /etc/apt/sources.list.d/kismet.list >/dev/null
-sudo apt update
-sudo apt install -y kismet
-
-#--------------
 # driver wifi
 #--------------
 sudo bash post/driver/wifi/rtl8188eus.sh
