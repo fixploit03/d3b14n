389123a90a0318b7ef5dd202961e7f0d1c3d8d91
fixploit03 <164528883+fixploit03@users.noreply.github.com>
2026-05-31 14:08:45 +0700

    Create rtl8814au.sh

---
+++ src/post/driver/wifi/rtl8814au.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+dependensi=(
+	"bc"
+	"build-essential"
+	"git"
+	"linux-headers-$(uname -r)"
+)
+
+apt-get update -y
+
+for d in "${dependensi[@]}"; do
+	apt-get install "${d}" -y
+done
+
+cd /usr/local/src
+git clone https://github.com/joseguzman1337/8814au
+cd 8814au
+
+make -j$(nproc)
+make install
+
+modprobe 8814au
