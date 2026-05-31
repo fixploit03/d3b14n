3013a91e8402f386215a3220a4a78e4aacf08c0f
fixploit03 <164528883+fixploit03@users.noreply.github.com>
2026-05-31 11:51:10 +0700

    Update setup_debian.sh

--- src/setup_debian.sh
+++ src/setup_debian.sh
@@ -2,11 +2,11 @@
 #
 # Debian 13 (Trixie)
 
-source cek_root.sh
-# bash konfigurasi_repositori.sh
-bash update_system.sh
-bash konfigurasi_sudo.sh
-bash konfigurasi_path_environment_variable.sh
-bash konfigurasi_network.sh
-bash konfigurasi_bashrc.sh
-bash konfigurasi_grub.sh
+source setup/cek_root.sh
+
+bash setup/update_system.sh
+bash setup/konfigurasi_sudo.sh
+bash setup/konfigurasi_path_environment_variable.sh
+bash setup/konfigurasi_network.sh
+bash setup/konfigurasi_bashrc.sh
+bash setup/konfigurasi_grub.sh
