da8e25ecda473dcb015552df143a8f2be3f50f26
fixploit03 <164528883+fixploit03@users.noreply.github.com>
2026-05-31 19:15:45 +0700

    Create instal_virtualbox_guest_additions.sh

---
+++ src/setup/instal_virtualbox_guest_additions.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+user=${SUDO_USER:-$USER}
+cdrom=$(blkid -t TYPE=iso9660 -o device | head -1)
+
+if [[ $(systemd-detect-virt) == "oracle" ]]; then
+    apt install -y linux-headers-$(uname -r) dkms perl
+
+    if [[ -z "${cdrom}" ]]; then
+        echo "[-] Tidak ada ISO yang terdeteksi!"
+        exit 1
+    fi
+
+    mount "${cdrom}" /mnt
+
+    if [[ -f /mnt/VBoxLinuxAdditions.run ]]; then
+        /mnt/VBoxLinuxAdditions.run
+        usermod -aG vboxsf "${user}"
+        umount /mnt
+    else
+        echo "[-] ISO Guest Additions belum di-insert!"
+        umount /mnt
+        exit 1
+    fi
+fi
