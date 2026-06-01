2de5f3009677cc26a8d0df906338d317b23d5aed
fixploit03 <164528883+fixploit03@users.noreply.github.com>
2026-06-01 07:05:25 +0700

    Update instal_utils.sh

--- src/setup/instal_utils.sh
+++ src/setup/instal_utils.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-apt install -y curl wget tree tmux rsync net-tools fastfetch htop btop plocate bash-completion build-essential
+apt install -y curl wget tree tmux rsync net-tools fastfetch htop btop plocate bash-completion build-essential xclip
