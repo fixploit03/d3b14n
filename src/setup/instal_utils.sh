de43c76fb9e188c65bfeff6991ac72aa66442eed
fixploit03 <164528883+fixploit03@users.noreply.github.com>
2026-06-01 09:57:11 +0700

    Add development libraries to installation script

--- src/setup/instal_utils.sh
+++ src/setup/instal_utils.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-apt install -y curl wget tree tmux rsync net-tools fastfetch htop btop plocate bash-completion build-essential xclip
+apt install -y curl wget tree tmux rsync net-tools fastfetch htop btop plocate bash-completion build-essential libnl-genl-3-dev libssl-dev pkg-config libpcap-dev xclip
