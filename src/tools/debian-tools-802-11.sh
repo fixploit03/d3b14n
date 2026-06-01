7f9dcbba479b3bc85784e2977b59b351fd612390
fixploit03 <164528883+fixploit03@users.noreply.github.com>
2026-06-01 09:55:17 +0700

    Update debian-tools-802-11.sh

--- src/tools/debian-tools-802-11.sh
+++ src/tools/debian-tools-802-11.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
 
+path_saat_ini=$(pwd)
 path_apps=../../apps/80211
+path_instal=/opt/tools/80211
+
+if [[ ! -d "${path_instal}" ]]; then
+    mkdir -p "${path_instal}"
+fi
 
 apt install -y wireless-tools iw aircrack-ng pixiewps reaver bully mdk3 mdk4 macchanger cowpatty hcxdumptool hcxtools hcxkeys bettercap wifite horst wavemon linssid
 
 # hostapd-wpe
 dpkg -i "${path_apps}/80211/libunsafessl1.0.2_1.0.2u-0kali2_amd64.deb" "${path_apps}/80211/hostapd-wpe_2.10+git20220310-0kali3_amd64.deb"
 apt install -y -f
+
+# hostapd-mana
+apt install -y build-essential git libnl-genl-3-dev libssl-dev pkg-config
+git clone https://github.com/sensepost/hostapd-mana "${path_instal}/hostapd-mana"
+cd hostapd-mana
+make -C hostapd -j$(nproc)
+mv hostapd/hostapd /usr/local/bin/hostapd-mana
+mv hostapd/hostapd_cli /usr/local/bin/hostapd-mana_cli
+ldconfig
+cd "${path_saat_ini}"
 
 # airgorah
 wget https://github.com/martin-olivier/airgorah/releases/download/v0.7.4/airgorah_0.7.4_x86_64.deb
