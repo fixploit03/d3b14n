e9a595a318032a90f2083970880855f0ef3d7ded
fixploit03 <164528883+fixploit03@users.noreply.github.com>
2026-06-07 13:32:49 +0700

    Create setup_debian.sh

---
+++ setup_debian.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e
+
+SCRIPT=(
+    "cek_root.sh"
+    "konfigurasi_path_environment_variable.sh"
+    "konfigurasi_network.sh"
+    "konfigurasi_bashrc.sh"
+    "update_system.sh"
+    "instal_sudo.sh"
+    "instal_utils.sh"
+    "instal_archiver.sh"
+    "instal_tampilan.sh"
+    "instal_terminal.sh"
+    "konfigurasi_grub.sh"
+)
+
+DIR=src/setup/
+
+for s in "${SCRIPT[@]}"; do
+    if [[ "${s}" == "cek_root.sh" || "${s}" == "konfigurasi_path_environment_variable.sh" ]]; then
+        source "${DIR}/${s}"
+    else
+        bash "${DIR}/${s}"
+    fi
+done
+
+echo -e "\n[+] Setup Debian selesai!"
