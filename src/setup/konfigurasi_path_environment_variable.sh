b4c3d3adce9276e93a3fc51adbd28bc820beaf22
fixploit03 <164528883+fixploit03@users.noreply.github.com>
2026-06-07 13:43:11 +0700

    Update konfigurasi_path_environment_variable.sh

--- src/setup/konfigurasi_path_environment_variable.sh
+++ src/setup/konfigurasi_path_environment_variable.sh
@@ -3,6 +3,7 @@
 config=/etc/profile.d/set-path.sh
 
 if [[ ! -f "${config}" ]]; then
+    echo "[*] Konfigurasi Path Environment Variable..."
     echo 'export PATH="$PATH:/usr/local/sbin:/usr/sbin:/sbin"' > "${config}"
     chmod +x "${config}"
 fi
